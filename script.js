a7b0b51728cbd74f839a3da8025e3c1a083c81f3
WahyuMina <117088938+WahyuMina@users.noreply.github.com>
2023-12-12 11:19:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll", function () {
   var header = this.document.querySelector("header");
   header.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+function ToggleAudio() {
+  var audio = document.getElementById("audio");
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+}
